a6a5ecb57bb039fe879b0e05d8a393025c343422
Tiny-Jenny <chizobanwosu199@gmail.com>
2023-08-10 16:34:13 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various types on the computer
+ * Return 0
+ */
+int main(void)
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("a, b, c, d, f: %lu byte(s)\n");
+return (0);
+}
