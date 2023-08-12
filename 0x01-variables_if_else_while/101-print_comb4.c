6aaf71046a09c75c42999170bfadec046d844b88
Tiny-Jenny <chizobanwosu199@gmail.com>
2023-08-11 18:15:04 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	 for (int i = 0; i < 7; i++) {
+        for (int j = i + 1; j < 8; j++) {
+            for (int k = j + 1; k < 9; k++) {
+                putchar(i + '0');
+                putchar(j + '0');
+                putchar(k + '0');
+                if (i != 6 || j != 7 || k != 8) {
+                    putchar(',');
+                    putchar(' ');
+		         }
+            }
+        }
+    }
+
+    return (0);
+}
