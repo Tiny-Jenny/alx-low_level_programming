d3f8e65a8da592e8722c88fae4ced2502a2f1962
Tiny-Jenny <chizobanwosu199@gmail.com>
2023-08-11 18:01:28 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 ((Success)
+ */
+int main(void)
+{
+	int ones = '0';
+	int tns = '0';
+	int hundreds = '0';
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens ='0'; tens <= '9'; tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{	
+				
+				if (!((ones == tn || (tens == humdred) ||
+						     (tens > ones || (hundred > tens)))
+
+							putchar(hundreds);
+							putchar(tense);
+							putchar(ones);
+							if (!(ones == '9' && hundreds == '7' && tens == '8'))
+							{
+								putchar(',');
+								putchar(' ')
