5c458acd340043b00a4324e385f8feae4542b0d2
Tiny-Jenny <chizobanwosu199@gmail.com>
2023-08-13 12:26:06 -0700

    Task 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,19 +5,23 @@
  */
 int main(void)
 {
-	 for (int i = 0; i < 7; i++) {
-        for (int j = i + 1; j < 8; j++) {
-            for (int k = j + 1; k < 9; k++) {
+	 for (int i = 0; i < 7; i++)
+	 {	 
+        for (int j = i + 1; j < 8; j++)
+	{	
+            for (int k = j + 1; k < 9; k++)
+	    {	    
                 putchar(i + '0');
                 putchar(j + '0');
                 putchar(k + '0');
-                if (i != 6 || j != 7 || k != 8) {
+                if (i != 6 || j != 7 || k != 8)
+		{	
                     putchar(',');
                     putchar(' ');
 		         }
             }
         }
-    }
-
+   }
+    putchar('\n');
     return (0);
 }
