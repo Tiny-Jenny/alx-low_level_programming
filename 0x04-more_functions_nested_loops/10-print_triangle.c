e6c001c5f7e207a7a2a4c7d6e606c99afc4c8c37
Tiny-Jenny <chizobanwosu199@gmail.com>
2023-08-17 19:30:49 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,15 +13,19 @@ void print_triangle(int size)
 		for (hash = 1; hash <= size; hash++)
 		{
 			for (index = size - hash; index > 0; index--)
-		{
-        		_putchar(' ');
+			{
+				
+        			_putchar(' ');
+			}
 			for (index = 0; index < hash; index++)
 			{
 				_putchar('#');
 			}
         	if (hash == size)
+		{
 			continue;
     		}
         	_putchar('\n');
 	}
+}
 }
