51d62883778e7ff9e8574d4235e3259a461b08f6
Tiny-Jenny <chizobanwosu199@gmail.com>
2023-08-21 14:57:32 -0700

    Task 1, 2, 3, 4

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: lenght
+ */
+int strlen(char *s)
+	int longi = 0;
+	while (*s != '\0)
+	{
+		longi++;
+		return (long1;)
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts - prints a string, followedd by a new line, tostdout
+ * @str: string to print
+ */
+void _puts (char *s)
+	while (*str != '\0')
+{
+	while (str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n*);
+}
