1d5d66683537c0c3360b52959d619a9464ce9977
Tiny-Jenny <chizobanwosu199@gmail.com>
2023-08-28 11:26:01 -0700

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,9 +10,9 @@ char *_strpbrk(char *s, char *accept)
 	int k;
 	while (*s)
 	{
-		for (k =0; accept[k]; k++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (*s ==accept[k])
+			if (*s == accept[k])
 				return (s);
 		}
 		s++
